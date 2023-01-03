ed442469482cf7a56928e731eb915f57bff1cfc1
Alabi12 <wolo22001@gmail.com>
2023-01-03 14:05:35 +0000

    add reducers to reserve and cancelled btn

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { reserveRocketsAction } from '../redux/rockets/rockets';
 
 const Rockets = () => {
+  const dispatch = useDispatch();
   const rocketsState = useSelector((state) => state.rockets);
 
   const rockets = [].concat(...rocketsState);
@@ -10,14 +12,27 @@ const Rockets = () => {
     <ul className="rocketsList">
       {rockets.map((rocket) => (
         <li className="rocketItem" key={rocket.id}>
-          <div><img className="rocketImage" src={rocket.flickr_images[0]} alt="rocket" /></div>
+          <div>
+            <img
+              className="rocketImage"
+              src={rocket.flickr_images[0]}
+              alt="rocket"
+            />
+          </div>
           <div className="rocketDetails">
             <h3 className="rocketName">{rocket.rocket_name}</h3>
             <div>
-              <i className="rocketStatus hide">Reserved</i>
+              {rocket.reserved ? <i className="rocketStatus">Reserved</i> : ''}
+
               <i className="rocketDescr">{rocket.description}</i>
             </div>
-            <button className="rocketBtn" type="button">Reserve Rocket</button>
+            <button
+              onClick={() => dispatch(reserveRocketsAction(rocket.id))}
+              className="rocketBtn"
+              type="button"
+            >
+              Reserve Rocket
+            </button>
           </div>
         </li>
       ))}
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,17 +1,36 @@
 const initialState = [];
 
 const GET_ROCKETS = 'space-travel/rockets/GET_ROCKETS';
+const RESERVE_ROCKETS = 'space-travel/rockets/RESERVE_ROCKETS';
+const CANCELLED_ROCKETS = 'space-travel/rockets/CANCELLED_ROCKETS';
 
 // Reducer
 export default function rocketsReducer(state = initialState, action = {}) {
+  const newState = state.map((rocket) => {
+    if (rocket.id !== action.id) return rocket;
+    return { ...rocket, reserved: true };
+  });
+
   switch (action.type) {
     case GET_ROCKETS:
       return [action.rockets];
-    default: return state;
+    case RESERVE_ROCKETS:
+      return [...state, newState];
+    case CANCELLED_ROCKETS:
+      return [action.rockets];
+    default:
+      return state;
   }
 }
 
 // Action Creators
 export function getRocketsAction(rockets) {
   return { type: GET_ROCKETS, rockets };
+}
+
+export function reserveRocketsAction(id) {
+  return { type: RESERVE_ROCKETS, id };
+}
+export function cancelledRocketsAction(id) {
+  return { type: CANCELLED_ROCKETS, id };
 }
